e1877530038bda0c675d2d0d4a492a3fdb5227f6
anglee <ang.lee@gmail.com>
2014-10-09 22:06:11 -0400

    Use process.env.PORT if possible

--- sync.js
+++ sync.js
@@ -6,4 +6,7 @@ app.get('/', function (req, res) {
   res.send("What do you sync?");
 });
 
-app.listen(5000);
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function () {
+  console.log("Sync app is running at port:" + app.get('port'));
+});
