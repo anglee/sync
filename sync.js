10382622c7856ebdaac5cefdd3aa82f5fa782563
anglee <ang.lee@gmail.com>
2014-10-09 22:29:07 -0400

    Add handler

--- sync.js
+++ sync.js
@@ -6,6 +6,14 @@ app.get('/', function (req, res) {
   res.send("What do you sync?");
 });
 
+var handler = function(req, res) {
+  res.write("req.method = " + req.method + "\n");
+  res.write("req.url =" + req.url + "\n");
+  res.end();
+};
+
+app.route('/foo').all(handler);
+
 app.set('port', (process.env.PORT || 5000));
 app.listen(app.get('port'), function () {
   console.log("Sync app is running at port:" + app.get('port'));
